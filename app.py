08dd2772f4e3f090561ebfc5fc5aa01709747c33
yourtechgirlife <ifetioluwawrites@gmail.com>
2025-05-13 18:26:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if uploaded_file:
 
 
         # Assuming 'label' is the target
-    X = data.drop("label", axis=1)
+X = data.drop("label", axis=1)
     y = data["label"]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
