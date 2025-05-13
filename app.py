767b0fee7b17cfe1b26448aba3110d859eccbdea
yourtechgirlife <ifetioluwawrites@gmail.com>
2025-05-13 18:28:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ y = data["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        model = LogisticRegression()
-        model.fit(X_train, y_train)
+model = LogisticRegression()
+model.fit(X_train, y_train)
 
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-        st.success(f"Model Accuracy: {acc:.2f}")
+st.success(f"Model Accuracy: {acc:.2f}")
