bf6957ec2d013d58c07dba21fb8fd76becf595d6
yourtechgirlife <ifetioluwawrites@gmail.com>
2025-05-13 18:28:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if uploaded_file:
 X = data.drop("label", axis=1)
 y = data["label"]
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         model = LogisticRegression()
         model.fit(X_train, y_train)
